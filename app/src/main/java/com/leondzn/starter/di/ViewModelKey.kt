656b8573f8442c925a7ffdbddd3525d052f607ac
Leonard Dizon <leonard@snekbyte.com>
2019-03-18 11:56:57 +0800

    Create ViewModelFactory for Dagger multibindings

---
+++ app/src/main/java/com/leondzn/starter/di/ViewModelKey.kt
@@ -0,0 +1,9 @@
+package com.leondzn.starter.di
+
+import android.arch.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
